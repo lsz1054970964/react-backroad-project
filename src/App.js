a1256f5a7a9643ddb8e9d194bebb2347db41d2fe
lsz1054970964 <lsz1054970964@gmail.com>
2023-03-04 13:35:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Narbar from "./components/Narbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+
+function App() {
+  return (
+    <>
+      <Narbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
